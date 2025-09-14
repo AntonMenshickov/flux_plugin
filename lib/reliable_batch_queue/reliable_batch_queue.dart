76d65a81403636af9c4c9f7c4cb6f8b3e32c8895
Anton M. <antonmenshickov@gmail.com>
2025-09-14 14:34:10 +0700

    Improved batch queue logic.

--- lib/reliable_batch_queue/reliable_batch_queue.dart
+++ lib/reliable_batch_queue/reliable_batch_queue.dart
@@ -1,9 +1,9 @@
 import 'dart:async';
+import 'dart:math';
 
 import 'package:flux_plugin/api/api.dart';
 import 'package:flux_plugin/model/event_message.dart';
 import 'package:hive/hive.dart';
-import 'package:uuid/uuid.dart';
 
 class ReliableBatchQueueOptions {
   final int batchSize;
@@ -19,24 +19,19 @@ class ReliableBatchQueue {
   static const String _queueBoxName = 'flux_logs_queue_box';
   static const String _processingBoxName = 'flux_logs_processing_box';
 
-  late final Uuid _uuid;
-
   late final Box<EventMessage> _queueBox;
   late final Box<EventMessage> _processingBox;
 
   late final int _batchSize;
   late final int _flushIntervalMs;
 
+  int _sequenceKey = 0;
   bool _flushing = false;
-  int _queueLen = 0;
-  int _processingLen = 0;
   Timer? _flushTimer;
 
   ReliableBatchQueue(ReliableBatchQueueOptions options)
     : _batchSize = options.batchSize,
-      _flushIntervalMs = options.flushIntervalMs {
-    _uuid = Uuid();
-  }
+      _flushIntervalMs = options.flushIntervalMs;
 
   Future<void> init() async {
     _queueBox = await Hive.openBox(_queueBoxName);
@@ -49,41 +44,55 @@ class ReliableBatchQueue {
   }
 
   Future<void> _restoreProcessing() async {
-    _processingLen = _queueBox.length;
-    _queueLen = _processingBox.length;
-    if (_processingLen > 0 || _queueLen > 0) {
+    final Iterable<MapEntry<int, EventMessage>> processingKeys = _processingBox
+        .toMap()
+        .entries
+        .cast();
+    for (var entry in processingKeys) {
+      if (_queueBox.get(entry.key) == null) {
+        await _queueBox.put(entry.key, entry.value);
+      }
+    }
+    await _processingBox.clear();
+    if (_queueBox.isNotEmpty) {
+      _sequenceKey = _queueBox.keys.cast<int>().reduce(max);
+    } else {
+      _sequenceKey = 0;
+    }
+    if (_processingBox.length > 0 || _queueBox.length > 0) {
       await _flush();
     }
   }
 
-  void addEvent(EventMessage event) {
-    _queueBox.put(_uuid.v4(), event);
-    _queueLen++;
+  void addEvent(EventMessage event) async {
+    await _queueBox.put(++_sequenceKey, event);
     _processQueue();
   }
 
   Future<void> _processQueue() async {
-    if (_queueLen >= _batchSize) {
+    if (_queueBox.length >= _batchSize) {
       await _flush();
     }
   }
 
   Future<Iterable<EventMessage>> _prepareBatch() async {
-    const List<EventMessage> events = [];
-    if (_processingLen > 0) {
+    final List<EventMessage> events = [];
+    if (_processingBox.length > 0) {
       events.addAll(_processingBox.values);
     } else {
-      final keys = _queueBox.toMap().keys.take(_batchSize);
-      for (String key in keys) {
+      final List<int> keys =
+          (_queueBox.toMap().keys.toList().cast<int>()
+                ..sort((a, b) => a.compareTo(b)))
+              .take(_batchSize)
+              .toList();
+      for (int key in keys) {
         final EventMessage? event = _queueBox.get(key);
         if (event != null) {
-          await _processingBox.add(event);
+          await _processingBox.put(key, event);
           await _queueBox.delete(key);
         }
       }
       events.addAll(_processingBox.values);
-      _queueLen -= events.length;
-      _processingLen += events.length;
     }
     return events;
   }
@@ -101,9 +110,8 @@ class ReliableBatchQueue {
     print('[ReliableBatchQueue] Flushing ${batch.length} messages');
 
     try {
-      Api.uploadEventsBatch(batch);
+      await Api.uploadEventsBatch(batch);
       await _processingBox.clear();
-      _processingLen = 0;
     } catch (err) {
       print(
         '[ReliableBatchQueue] error while flushing messages to database\n$err',
